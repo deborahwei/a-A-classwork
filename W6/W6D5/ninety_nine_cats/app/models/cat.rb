8578230a15f76a9292b1fd2f1d83738a61247975
Deborah Wei <dew354@nyu.edu>
2022-07-03 13:29:07 -0400

    Finished most of phase 1

--- W6/W6D5/ninety_nine_cats/app/models/cat.rb
+++ W6/W6D5/ninety_nine_cats/app/models/cat.rb
@@ -1,12 +1,13 @@
 require 'action_view'
 
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
 
-    validates :color, inclusion: { in: %w(red, orange, ginger, yellow, brown, white, black, grey)}
-    validates :sex, inclusion: { in: %w(M, F)}
-    # include ActionView::Helpers::DateHelper
+    validates :color, inclusion: { in: %w(red orange ginger yellow brown white black grey)}
+    validates :sex, inclusion: { in: %w(m M f F)}
+
     # def age
-    #     current_date = Date.now
+    #     current_date = Date.now.to_date
     #     time_diff = ((current_date - self.birth_date)//365).to_i
     #     return time_diff 
     #     end
--- W6/W6D5/ninety_nine_cats/db/seeds.rb
+++ W6/W6D5/ninety_nine_cats/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.destroy_all
+Cat.create!(birth_date: '2016/02/19', color: "red", name: "Claudia", description: "Is a bad cat", sex: "m")
+Cat.create!(birth_date: '2012/12/21', color: "orange", name: "Clarence", description: "Is a good cat", sex: "f")
+Cat.create!(birth_date: '2011/10/09', color: "grey", name: "Cameron", description: "Is a loud cat", sex: "m")
+Cat.create!(birth_date: '2010/08/03', color: "black", name: "Charles", description: "Is a fat cat", sex: "f")
+Cat.create!(birth_date: '2009/06/12', color: "yellow", name: "ChaCha", description: "Is a small cat", sex: "m")
