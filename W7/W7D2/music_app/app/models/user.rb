6fef6a110bea7a27e9ecf527e521c6457572bb89
Deborah Wei <dew354@nyu.edu>
2022-07-06 11:32:34 -0400

    Finished fig vaper

--- W7/W7D2/music_app/app/models/user.rb
+++ W7/W7D2/music_app/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
 
+    attr_reader :password 
+    after_initalize: :ensure_session_token
+
     validates :email, presence: true 
+    validates :password, length: {minimum: 6}, allow_nil: true 
 
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
@@ -21,4 +25,17 @@ class User < ApplicationRecord
         SecureRandom::urlsafe_base64
     end
 
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        session_token ||= SecureRandom::urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
 end
