f8c000a5f9aa7604bffac61599f50f9e600e7356
Deborah Wei <dew354@nyu.edu>
2022-07-06 10:40:51 -0400

    Created music_app

--- W7/W7D1/ninety_nine_cats_2/app/controllers/cat_rental_requests_controller.rb
+++ W7/W7D1/ninety_nine_cats_2/app/controllers/cat_rental_requests_controller.rb
@@ -6,6 +6,7 @@ class CatRentalRequestsController < ApplicationController
 
   def create
     @rental_request = CatRentalRequest.new(cat_rental_request_params)
+
     if @rental_request.save
       redirect_to cat_url(@rental_request.cat)
     else
