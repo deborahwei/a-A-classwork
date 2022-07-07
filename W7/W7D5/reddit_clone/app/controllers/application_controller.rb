c7cf1854e559117dc41d90a1aa87e3f5e0e2e56d
Deborah Wei <dew354@nyu.edu>
2022-07-07 19:50:47 -0400

    Auth

--- W7/W7D5/reddit_clone/app/controllers/application_controller.rb
+++ W7/W7D5/reddit_clone/app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+
+    def current_user 
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def require_logged_in 
+        redirect_to new_session_url unless logged_in? 
+    end
+
+    def require_logged_out 
+        redirect_to new_session_url if logged_in? 
+    end
+
+    def logged_in? 
+        !!current_user 
+    end
+
+    def login(user)
+        @current_user = user
+        session[:session_token] = user.session_token
+    end
+
+    def logout 
+        user.reset_session_token
+        session[:session_token] = nil
+    end
 end
--- W7/W7D5/reddit_clone/app/models/user.rb
+++ W7/W7D5/reddit_clone/app/models/user.rb
@@ -1,2 +1,44 @@
 class User < ApplicationRecord
+
+    attr_reader :password 
+    after_initialization :ensure_session_token
+
+    validates :username, :session_token, presence: true, uniqueness: true 
+    validates :password_digest, presence: true 
+    validates :password, length: { minimum: 6 }, allow_nil: true 
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            return user
+        else 
+            return nil
+        end
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def generate_token_session 
+        SecureRandom::urlsafe_base64
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token 
+        self.session_token ||= generate_token_session
+    end
+
+    def reset_session_token 
+        self.session_token = generate_token_session
+        self.save 
+        self.session_token
+    end
+
+
 end
